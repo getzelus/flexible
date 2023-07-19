a347907861e2543a2e33ae098274de109315fe9d
getzelus <chamoloss@live.fr>
2023-07-19 12:11:34 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+
+
+const Home = async () => {
+ 
+
+  return (
+    <section className="flexStart flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load more</h1>
+
+      </section>
+
+     
+  )
+};
+
+export default Home;
